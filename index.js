acc5eb4168644a8c1b29bd8d15986c046902b239
vishal pandhare <vishalpandhare2016@gmail.com>
2024-03-13 15:00:32 +0530

    search api and product detial page function created

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 /*Get api with product data*/
-
+// https://dummyjson.com/products/search?q=phone
 let num = 0;
-async function getData(skip, limit) {
+let totalItem = 0;
+async function getData(skip) {
+  let limit = 4;
   const response = await fetch(
     `https://dummyjson.com/products?skip=${skip}&limit=${limit}`
   );
   const data = await response.json();
   let col = document.getElementById("col");
-
-  if (data.products.length === data.total) {
-    return;
-  }
-
+  col.innerHTML = "";
   data.products.forEach((el) => {
     let newElement = document.createElement("div");
     newElement.className = "card";
@@ -24,6 +22,7 @@ async function getData(skip, limit) {
     newButton.textContent = "ADD TO CART";
     newButton.className = "addToCart";
     newImg.src = el.thumbnail;
+    newImg.id = "productImg";
     price.textContent = "Price " + el.price;
     title.textContent = el.title;
     newElement.appendChild(newButton);
@@ -31,12 +30,21 @@ async function getData(skip, limit) {
     newElement.appendChild(title);
     newElement.appendChild(price);
     col.appendChild(newElement);
+    newElement.addEventListener("click", function () {
+      localStorage.setItem("thumbnail", el.thumbnail);
+      localStorage.setItem("price", el.price);
+      localStorage.setItem("title", el.title);
+      localStorage.setItem("description", el.description);
+      console.log(el);
+      location.replace("details.html");
+    });
   });
+  totalItem = data.total;
   console.log(data);
   return;
 }
-getData(num, 12);
 
+getData(0);
 /* menu bar button function */
 function openMenu() {
   console.log("work");
@@ -62,8 +70,111 @@ function openMenu() {
   }
 }
 
-/*pagination*/
-function Next() {
-  num++;
-  getData(num, 12);
+/*pagination number movement*/
+function nextPage() {
+  let btn1 = document.getElementById("0");
+  let btn2 = document.getElementById("1");
+  let btn3 = document.getElementById("2");
+  let btn4 = document.getElementById("3");
+  let btn5 = document.getElementById("4");
+
+  console.log("totalItem", totalItem, parseInt(btn5.textContent));
+  if (totalItem <= parseInt(btn5.textContent)) {
+    return;
+  }
+
+  btn1.textContent = parseInt(btn1.textContent) + 5;
+  btn2.textContent = parseInt(btn2.textContent) + 5;
+  btn3.textContent = parseInt(btn3.textContent) + 5;
+  btn4.textContent = parseInt(btn4.textContent) + 5;
+  btn5.textContent = parseInt(btn5.textContent) + 5;
 }
+
+function prevPage() {
+  let btn1 = document.getElementById("0");
+  let btn2 = document.getElementById("1");
+  let btn3 = document.getElementById("2");
+  let btn4 = document.getElementById("3");
+  let btn5 = document.getElementById("4");
+
+  if (parseInt(btn1.textContent) === 0) {
+    return;
+  }
+
+  btn1.textContent = parseInt(btn1.textContent) - 5;
+  btn2.textContent = parseInt(btn2.textContent) - 5;
+  btn3.textContent = parseInt(btn3.textContent) - 5;
+  btn4.textContent = parseInt(btn4.textContent) - 5;
+  btn5.textContent = parseInt(btn5.textContent) - 5;
+}
+
+/*pagination buttons*/
+
+document.getElementById("0").addEventListener("click", () => {
+  let num = document.getElementById("0").textContent;
+  getData(parseFloat(num));
+});
+document.getElementById("1").addEventListener("click", () => {
+  let num = document.getElementById("1").textContent;
+  getData(parseFloat(num));
+});
+document.getElementById("2").addEventListener("click", () => {
+  let num = document.getElementById("2").textContent;
+  getData(parseFloat(num));
+});
+document.getElementById("3").addEventListener("click", () => {
+  let num = document.getElementById("3").textContent;
+  getData(parseFloat(num));
+});
+document.getElementById("4").addEventListener("click", () => {
+  let num = document.getElementById("4").textContent;
+  getData(parseFloat(num));
+});
+
+/* search input function*/
+
+document
+  .getElementById("searchForm")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+    var searchInputValue = document.getElementById("searchInput").value;
+    const response = await fetch(
+      `https://dummyjson.com/products/search?q=${searchInputValue}`
+    );
+    const data = await response.json();
+    let col = document.getElementById("col");
+    col.innerHTML = "";
+    data.products.forEach((el) => {
+      let newElement = document.createElement("div");
+      newElement.className = "card";
+      newElement.id = "card";
+      let newButton = document.createElement("button");
+      let newImg = document.createElement("img");
+      let price = document.createElement("p");
+      price.className = "price";
+      let title = document.createElement("p");
+      newButton.textContent = "ADD TO CART";
+      newButton.className = "addToCart";
+      newImg.src = el.thumbnail;
+      price.textContent = "Price " + el.price;
+      title.textContent = el.title;
+      newElement.appendChild(newButton);
+      newElement.appendChild(newImg);
+      newElement.appendChild(title);
+      newElement.appendChild(price);
+      col.appendChild(newElement);
+      newElement.addEventListener("click", function () {
+        localStorage.setItem("thumbnail", el.thumbnail);
+        localStorage.setItem("price", el.price);
+        localStorage.setItem("title", el.title);
+        localStorage.setItem("description", el.description);
+        console.log(el);
+        location.replace("details.html");
+      });
+    });
+    totalItem = data.total;
+    console.log(data);
+    return;
+  });
+
+console.log(document.getElementById("card"));
